88cb5f28b726bd1deb8d69b61850f4f1c207ddf4
tanghoong <tanghoong.com@gmail.com>
2024-09-27 22:58:55 +0800

    Update path

--- docs/build/_shared/remix_hmr-KM4OE53Z.js
+++
@@ -1,9 +0,0 @@
-import {
-  createHotContext
-} from "/docs/build/_shared/chunk-WBV36VXN.js";
-import "/docs/build/_shared/chunk-QXIHW5KQ.js";
-import "/docs/build/_shared/chunk-6RDTFI4Q.js";
-export {
-  createHotContext
-};
-//# sourceMappingURL=/docs/build/_shared/remix_hmr-KM4OE53Z.js.map
--- docs/build/_shared/runtime-OOL6MXXX.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_runtime
-} from "/docs/build/_shared/chunk-QXIHW5KQ.js";
-import "/docs/build/_shared/chunk-6RDTFI4Q.js";
-export default require_runtime();
-//# sourceMappingURL=/docs/build/_shared/runtime-OOL6MXXX.js.map
